3314358af32aed7a06194b94a47fd01dfe0602ad
Darya Fedorova <dasha_f_2001@mail.ru>
2022-11-18 19:59:38 +0300

    feat: popup version 2. change name and description

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,57 +1,45 @@
 let popupOpenButton = document.querySelector('.profile__edit-btn');
 let popupCloseButton = document.querySelector('.popup__close-btn');
+
 let popup = document.querySelector('.popup');
-let popupInput = document.querySelectorAll('.popup__input');
+let popupForm = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let descriptionInput = document.querySelector('.popup__input_type_description');
 
-function InputFilling() {
-  for (let i = 0; i < popupInput.length; i++) {
-    if (popupInput[i].name === 'name') {
-      let profileName = document.querySelector('.profile__name').textContent;
-      popupInput[i].value = profileName;
-    } else 
-    
-    if (popupInput[i].name === 'description') {
-      let profileDescription = document.querySelector('.profile__description').textContent;
-      popupInput[i].value = profileDescription;
-    }
+let nameProfile = document.querySelector('.profile__name');
+let descriptionProfile = document.querySelector('.profile__description');
+
+
+// вставляет в поля формы значения из профиля
+
+function FillInputValues() {    
+  nameInput.value = nameProfile.textContent;
+  descriptionInput.value = descriptionProfile.textContent;
+}
+
+// открывает и закрывает попап
+
+function OpenClosePopup() {   
+  popup.classList.toggle('popup_opened');
+
+  if (!popup.classList.contains('.popup_opened')) {
+    FillInputValues();
   }
 }
 
-function OpenClosePopup() {
-  if (popup.classList.contains('popup_opened')) {
-    popup.classList.remove('popup_opened');
-  } else {
-    InputFilling();
-    popup.classList.add('popup_opened');
-  }
+// отправка формы и заполнение полей порофиля новыми значениями
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+    nameProfile.textContent = nameInput.value;
+    descriptionProfile.textContent = descriptionInput.value;
+
+    OpenClosePopup();
 }
-
-
 
 popupOpenButton.addEventListener('click', OpenClosePopup);
 popupCloseButton.addEventListener('click', OpenClosePopup);
 
+popupForm.addEventListener('submit', formSubmitHandler);
 
-// Находим форму в DOM
-let popupForm = document.querySelector('popup__form');
-// Находим поля формы в DOM
-let nameInput = // Воспользуйтесь инструментом .querySelector()
-let jobInput = // Воспользуйтесь инструментом .querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
